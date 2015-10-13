f83ef085b185b1164860aeb2bd52c3087f322fc7
dizer <dizeru@gmail.com>
2015-10-13 14:07:55 +0700

    updated telegram spec

--- lib/telegraphist/model/user.rb
+++ lib/telegraphist/model/user.rb
@@ -1,6 +1,6 @@
 module Telegraphist
   class Model::User < Model
-    attr_accessor :id, :first_name, :last_name, :username
+    attr_accessor :id, :first_name, :last_name, :username, :type
 
     def to_s
       [id, username, [first_name, last_name].compact.join(' ')].join(' :: ')
